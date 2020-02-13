70cef4c3afc42bb3e19c82d785e50636f9ceaf1c
Ofek Inbar <ofekinbar@hotmail.com>
2020-02-13 14:57:20 -0800

    Link up new comments to camera Fix panning bug

--- src/ModelRenderer2.js
+++ src/ModelRenderer2.js
@@ -1,7 +1,7 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useRef, useState } from 'react';
 import * as THREE from 'three';
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { Canvas, useThree, extend, useLoader } from 'react-three-fiber';
+import { Canvas, useThree, extend, useLoader, useFrame } from 'react-three-fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 extend({ OrbitControls });
@@ -11,16 +11,39 @@ function Model() {
   return <primitive object={gltf.scene} position={[0, 0, 0]} />
 }
 
-function Controls(props) {
+function Controls({ cameraPosition, onUpdate, setOrbitControls, ...props }) {
+  const [controlsSet, setControlsSet] = useState(true);
   const {
     camera,
     gl: { domElement }
   } = useThree();
+  const controls = useRef();
+  
+  useFrame(({ camera }) => {
+    if (controlsSet) {
+      setOrbitControls(controls.current);
+      setControlsSet(false);
+    }
 
-  return <orbitControls args={[camera, domElement]} {...props} />
+    if (cameraPosition) {
+      camera.position.set(...cameraPosition.position);
+      camera.lookAt(0, 0, 0);
+      controls.current.target.set(...cameraPosition.focus);
+      controls.current.update();
+      onUpdate();
+    }
+  });
+
+  return <orbitControls ref={controls} args={[camera, domElement]} {...props} />
 }
 
-export function ModelRenderer2({ setCameraPosition, ...props }) {
+export function ModelRenderer2({
+  cameraPosition,
+  onUpdate,
+  setCamera,
+  setOrbitControls,
+  ...props
+}) {
   return <Canvas camera={{ fov: 60, near: 1, position: [-2, 0, -3] }}
     gl={{ outputEncoding: THREE.sRGBEncoding }}
     onCreated={({ gl, scene, camera }) => {
@@ -28,13 +51,15 @@ export function ModelRenderer2({ setCameraPosition, ...props }) {
       scene.background = new THREE.Color(0xfafafa);
       camera.lookAt(0, 0, 0);
 
-      setCameraPosition(position => {
-        camera.position.set(...position);
-        camera.lookAt(0, 0, 0);
-      });
+      setCamera(camera);
     }}
     {...props}>
-    <Controls minDistance={1} maxDistance={500} />
+    <Controls cameraPosition={cameraPosition}
+      onUpdate={onUpdate}
+      setOrbitControls={setOrbitControls}
+      minDistance={1}
+      maxDistance={50}
+    />
     <pointLight position={[10, 10, 10]} />
     <pointLight position={[-10, -10, -10]} />
     <Suspense fallback={null}>
