d5c46d2de7beff9c4b165f4d0c4b6db0d08b8a02
Ofek Inbar <ofekinbar@hotmail.com>
2020-02-14 11:51:47 -0800

    Modify Note component to support threads

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -1,37 +1,59 @@
 import React from 'react';
 import './Note.css';
 
+function replyString(thread) {
+  if (thread && thread.length > 0) {
+    if (thread.length > 1) {
+      return `${thread.length} Replies`;
+    } else {
+      return '1 Reply';
+    }
+  } else {
+    return 'Reply';
+  }
+}
+
 export function Note({
   selected,
   white,
   author,
   comment,
   profileColor,
+  thread,
   onDelete,
+  onReply,
+  showReply=true,
   ...props
 }) {
   return <div className="wrapper">
-      <div className={selected ? 'selected-bar' : ''}></div>
-      <div className={'note' + (white ? ' white' : '')} {...props}>
-        <div className="content-row">
-          <div className="profile-image" style={{backgroundColor: profileColor}}></div>
-          <div className="content-column">
-            <div className="author-row">
-              <div className="author-name">{author}</div>
-              <i className="close-button material-icons"
-                onClick={event => {
-                  event.stopPropagation();
-                  onDelete();
-                }}>
-                close
+    <div className={selected ? 'selected-bar' : ''}></div>
+    <div className={'note' + (white ? ' white' : '')} {...props}>
+      <div className="content-row">
+        <div className="profile-image" style={{ backgroundColor: profileColor }}></div>
+        <div className="content-column">
+          <div className="author-row">
+            <div className="author-name">{author}</div>
+            <i className="close-button material-icons"
+              onClick={event => {
+                event.stopPropagation();
+                onDelete();
+              }}>
+              close
               </i>
-            </div>
-            <div className="comment">{comment}</div>
-            <div className="reply">
-              <span className="reply-button">Reply</span>
-            </div>
           </div>
+          <div className="comment">{comment}</div>
+          {props.children}
+          {showReply ? <div className="reply">
+            <span className="reply-button"
+              onClick={event => {
+                event.stopPropagation();
+                onReply()
+              }}>
+              {replyString(thread)}
+            </span>
+          </div> : null}
         </div>
       </div>
-    </div>;
+    </div>
+  </div>;
 }
