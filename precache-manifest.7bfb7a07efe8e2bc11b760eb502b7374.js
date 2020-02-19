ec8b575989defe9fe4c98d76a5fc021a147ecc95
Carson Dauber <carsondauber@gmail.com>
2020-02-19 11:39:49 -0800

    Updates

---
+++ precache-manifest.7bfb7a07efe8e2bc11b760eb502b7374.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "865600677f59499d32611f755143f65d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5dcd307cd43f076cfc61",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "4d950f2c7e7110f55a67",
+    "url": "/static/css/main.96ae1361.chunk.css"
+  },
+  {
+    "revision": "5dcd307cd43f076cfc61",
+    "url": "/static/js/2.9ed46f61.chunk.js"
+  },
+  {
+    "revision": "76379644efda569a9fb03ad0e95991cd",
+    "url": "/static/js/2.9ed46f61.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4d950f2c7e7110f55a67",
+    "url": "/static/js/main.583beab2.chunk.js"
+  },
+  {
+    "revision": "11b262c79afc1b34a0b4",
+    "url": "/static/js/runtime-main.059fb6ce.js"
+  }
+]);
