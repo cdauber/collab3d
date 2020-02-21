d7a02f081ba241f6d6493e1d041087fc6eafea0f
ASUS <jiajis2@cs.washington.edu>
2020-02-20 20:10:27 -0800

    color changde. Icon exploring

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,35 +1,41 @@
 import React from "react";
 import "./Header.css";
+import {FaComment} from 'react-icons/fa';
+import { icons } from "react-icons/lib/cjs";
 
 class Header extends React.Component {
   render() {
-    return (<div className="head">
-      <div className="greeting-row">
-        <span className="greeting">Welcome Username!</span>
+    return (     
+    <div className="head">
+      <div className="back-row">
+        <span className="back">Back</span>
       </div>
-      <div className="button-row">
-        <div className="buttons">
-          <button
-            className="share"
-            onClick={() => alert('share link copied')}>
-            <span className="textShare">
-              Share
-         </span>
-          </button>
-          <button
-            className="AR"
+      <div className="greeting-row">
+        <span className="greeting">Prototype 2</span>
+      </div>
+      <div className="button-row" >
+          <button className="comment">
+          <FaComment className="iconComment" />
+          </button>   
+        <button className="AR"
             onClick={() => alert('more features to come')}>
             <span className="textAR">
               AR
             </span>
           </button>
-        </div>
-        <img className="profile"
+          <button className="share"
+            onClick={() => alert('share link copied')}>
+            <span className="textShare">
+              Share
+            </span>
+          </button>
+          <img className="profile"
              alt = "Greg"
              url src = "https://assets.website-files.com/5d3152346d95065922960b3a/5db769036b1dff3418bffd20_Greg.jpg"
         >           
-        </img>
-      </div>
+        </img>  
+        </div>
+            
     </div>);
   }
 }
