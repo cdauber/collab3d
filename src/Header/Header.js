b61cefc782819b4c4173829e6e35d5f1ce820498
ASUS <jiajis2@cs.washington.edu>
2020-02-21 15:15:36 -0800

    No arrow/ waiting finalizing on dropdown clickings

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -35,10 +35,10 @@ class Header extends React.Component {
               <FaNeuter className="iconPin" size="1.8em"
               />  Pin</span>
             <span className="dpSpan"
-              onClick={() => document.getElementById("checker").classList.toggle("show")}>
+              onClick={() => document.getElementById("checker3").classList.toggle("show")}>
               <FaCheck id="checker3" className="checker3"/>
               <RiPencilLine className="iconPen" size="1.8em"/>  Drawover</span>
-          </div>
+             </div>
           </button>   
         <button className="AR"
             onClick={() => alert('more features to come')}>
@@ -69,16 +69,34 @@ toggle between hiding and showing the dropdown content */
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.topComment')&&
-  !event.target.matches('.iconComment')&&
-  !event.target.matches('.iconAngle')&&
-  !event.target.matches('dropdown-content')
+      !event.target.matches('.iconComment')&&
+     !event.target.matches('.iconAngle')&&
+     !event.target.matches('dropdown-content')
   ) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
+    var check1 = document.getElementsByClassName("checker1");
+    var check2 = document.getElementsByClassName("checker2");
+    var check3 = document.getElementsByClassName("checker3");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
+      }
+    }for (i = 0; i < dropdowns.length; i++) {
+      var openCheck1 = check1[i];
+      if (openCheck1.classList.contains('show')) {
+        openCheck1.classList.remove('show');
+      }
+    }for (i = 0; i < dropdowns.length; i++) {
+      var openCheck2 = check2[i];
+      if (openCheck2.classList.contains('show')) {
+        openCheck2.classList.remove('show');
+      }
+    }for (i = 0; i < dropdowns.length; i++) {
+      var openCheck3 = check3[i];
+      if (openCheck3.classList.contains('show')) {
+        openCheck3.classList.remove('show');
       }
     }
   }
