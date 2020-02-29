f8b0f929f3cf8b6a87323ea44709892d7d23e673
Ofek Inbar <ofekinbar@hotmail.com>
2020-02-29 09:52:09 -0800

    * Remove react-icons dependency * Add eye icon(s) * Simplify HTML/CSS

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,37 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 
-class Header extends React.Component {
-  render() {
-    return (     
+function Header() {
+  const [eyeOpen, setEyeOpen] = useState(true);
+
+  return (
     <div className="head">
-      <div className="back-row">
-        <span className="back">Back</span>
+      <button className="back-button">Back</button>
+      <div className="button-row">
+        <img
+          className="profile"
+          alt="Greg"
+          src="https://assets.website-files.com/5d3152346d95065922960b3a/5db769036b1dff3418bffd20_Greg.jpg"
+        />
+        <button className="AR" onClick={() => alert("more features to come")}>
+          AR
+        </button>
+        <button className="share" onClick={() => alert("share link copied")}>
+          Share
+        </button>
+        <button className="eye" onClick={() => setEyeOpen(!eyeOpen)}>
+          <img
+            src={`assets/eye-${eyeOpen ? "open" : "closed"}.svg`}
+            alt="Show/hide pins icon"
+          />
+        </button>
       </div>
       <div className="greeting-row">
-        <span className="greeting">Prototype 2</span>
+        <span>Nike Zoom Prototype 1</span>
       </div>
-      <div className="button-row" >
-        <button className="AR"
-            onClick={() => alert('more features to come')}>
-            <span className="textAR">
-              AR
-            </span>
-          </button>
-          <button className="share"
-            onClick={() => alert('share link copied')}>
-            <span className="textShare">
-              Share
-            </span>
-          </button>
-        </div>
-        <img className="profile"
-             alt = "Greg"
-             url src = "https://assets.website-files.com/5d3152346d95065922960b3a/5db769036b1dff3418bffd20_Greg.jpg"
-        >           
-        </img>      
-    </div>);
-  }
+    </div>
+  );
 }
 
 export default Header;
