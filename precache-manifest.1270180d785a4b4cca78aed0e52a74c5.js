204c87a8100c8a7689e49c7ffa76c5c96c65b3b9
Ofek Inbar <ofekinbar@hotmail.com>
2020-02-28 19:27:42 -0800

    Updates

---
+++ precache-manifest.1270180d785a4b4cca78aed0e52a74c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27e02b8858b8514c46ce015afbcc72cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa1e08f5362caacb0f6a",
+    "url": "/static/css/main.c70b82a7.chunk.css"
+  },
+  {
+    "revision": "ec5aca8565e6b2c21e83",
+    "url": "/static/js/2.153a3200.chunk.js"
+  },
+  {
+    "revision": "1d209a8195b982853d75349d3fbbacd9",
+    "url": "/static/js/2.153a3200.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa1e08f5362caacb0f6a",
+    "url": "/static/js/main.b8311cf7.chunk.js"
+  },
+  {
+    "revision": "11b262c79afc1b34a0b4",
+    "url": "/static/js/runtime-main.059fb6ce.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8d6c33afa6ac4d6f1f718dbb65c6c3c8.js"
+  "/precache-manifest.1270180d785a4b4cca78aed0e52a74c5.js"
 );
 
 self.addEventListener('message', (event) => {
